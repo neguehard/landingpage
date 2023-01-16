41dbc98c4e27797cfd76fdaac5f3aa1349ad09b8
neguehard <aurelien.enguehard@epitech.eu>
2023-01-16 13:43:23 +0100

    push modif login

--- flylens-landing/src/App.vue
+++ flylens-landing/src/App.vue
@@ -30,7 +30,7 @@
               <a class="nav-link js-scroll-trigger" href="#faq">FAQ</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="https://flylenstest.online/">fr</a>
+              <a class="nav-link js-scroll-trigger" href="https://flylenstest.online/">Login</a>
             </li>
           </ul>
         </div>
